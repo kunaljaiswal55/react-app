6a342e10bd944633214c9e9c5919c871280cef72
Kunal Jaiswal <jaiswalkunal555@gmail.com>
2026-07-21 23:56:36 +0530

    let-and-call

--- src/hello.jsx
+++ src/hello.jsx
@@ -1,8 +1,10 @@
 function Hello(){
+  let Myname= "kunal";
   return (
     <div>
     <h3>I Am Macking React Components and working on inport and export </h3>
     <h4>The Next Task IS To Mack It Dynamic.</h4>
+    <h4>hy i am {Myname}</h4>
     </div>
   );
 }
